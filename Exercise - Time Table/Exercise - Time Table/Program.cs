b4e5fa3e162ab74b4439c088e333f39d997a3235
Tobias <145202239+CodeNikaa@users.noreply.github.com>
2025-01-29 09:23:07 +0100

    Add files via upload

---
+++ Exercise - Time Table/Exercise - Time Table/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TimesTable_Exercise
+{
+    class timesTable_Exercise
+    {   
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number for TimeTable: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine("{0} * {1} = {2}", i, userInput, i * userInput);
+            }
+        }
+    }
+}
---
+++ Exercise-PasswordChecker/Exercise-PasswordChecker/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace pwchecker
+{
+    class PWChecker
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter password: ");
+            string password = Console.ReadLine();
+            Console.WriteLine(password);
+
+            Console.WriteLine("Enter password again: ");
+            string passwordCheck = Console.ReadLine();
+            Console.WriteLine(passwordCheck);
+
+            if (!password.Equals(string.Empty))
+            {
+                if (!passwordCheck.Equals(string.Empty))
+                {
+                    if (password.Length >= 10 && passwordCheck.Length >= 10)
+                    {
+                        if (password.Equals(passwordCheck))
+                        {
+                            Console.WriteLine("Password is Valid!");
+                        } 
+                        else
+                        {
+                            Console.WriteLine("Password is inValid");
+                        }
+
+                    }
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Please enter a password: ");
+            }
+
+        }
+    }
+}  
