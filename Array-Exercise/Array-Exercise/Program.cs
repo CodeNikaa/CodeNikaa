35d88e2c5f6c562cc852f532dc8399a85be942a7
Tobias <145202239+CodeNikaa@users.noreply.github.com>
2025-01-29 09:21:15 +0100

    Add files via upload

---
+++ Array-Exercise/Array-Exercise/Program.cs
@@ -0,0 +1,26 @@
+﻿// Here your Exercise will go in
+// The goal are:
+// to create an array where the user can store 5+ integers
+// - ask the user to enter the amount of integers into the array
+// - sort the integers from lowest-highest numbers first independent on how the users entered the numbers
+// - print them out to the console
+// - then reverse the array and also print it out (just for practice)
+// - after the array is sorted, give the user an opportunity to search for their inputs
+// - then print their searched inputs + position their at
+
+using System;
+
+namespace ArrayExercise {
+    class Program {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
+
+
+
+
---
+++ Array.Sort.Reverse/Array.Sort.Reverse/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = new int[]
+            {
+                0, 1, 2, 3, 4, 5
+            };
+
+            //Array.Sort(numbers);
+
+            foreach (var num in numbers)
+            {
+                Console.Write($"{num} ");
+            }
+
+            int[] sortNumbers = new int[numbers.Length];
+
+            int x = 0;
+
+            Console.WriteLine("");
+
+            for (int i = numbers.Length -1; i >= 0; i--)
+            {
+                sortNumbers[x] = numbers[i];
+                x++;
+            }
+
+            foreach (var num in sortNumbers)
+            {
+                Console.Write($"{num} ");
+            }
+        }
+    }
+}
